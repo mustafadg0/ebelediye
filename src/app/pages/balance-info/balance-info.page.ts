25267138ff5f7727cd263d02fad5aea4e34a3559
Mustafa Daglilar <m.daglilar@hotmail.com>
2024-09-02 01:35:25 +0300

    fx

--- src/app/pages/balance-info/balance-info.page.ts
+++ src/app/pages/balance-info/balance-info.page.ts
@@ -22,7 +22,6 @@ export class BalanceInfoPage implements OnInit {
 
   handleRefresh(event : any) {
     setTimeout(() => {
-      // Any calls to load data go here
       event.target.complete();
     }, 2000);
   }
